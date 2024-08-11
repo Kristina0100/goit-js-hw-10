e0db2ea4765bffb4adf74d039a01575759ca1ddd
Kristina0100 <156364109+Kristina0100@users.noreply.github.com>
2024-08-11 00:29:51 +0000

    Deploying to gh-pages from @ Kristina0100/goit-js-hw-10@a8f269beecb653335cd2493cebe12626c7958dd4 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";const t=document.querySelector("button");t.addEventListener("submit",onBtnSubmit);
+//# sourceMappingURL=commonHelpers2.js.map
